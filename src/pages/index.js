139bcd4f4f6f366cfd0e65ed97dcc6749f427fe3
Afiliazon <info@afiliazon.com>
2020-03-30 18:19:45 +0200

    esto

--- src/pages/index.js
+++ src/pages/index.js
@@ -771,15 +771,13 @@ const IndexPage = () => (
             <ul className="nav nav-tabs location-tab justify-content-center" role="tablist">
               <li className="nav-item">
                 <a className="nav-link active" data-toggle="tab" href="#location-tab-1">Los Angeles</a>
-              </li>
-              
+              </li>     
             </ul>
             {/* End of .nav-tabs */}
             <div className="tab-content location-tab-content grid-wrapper" id="myTabContent">
               <div className="tab-pane fade show active" id="location-tab-1">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d423283.4355495124!2d-118.69191514099776!3d34.020730497983934!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c75ddc27da13%3A0xe22fdf6f254608f4!2sLos+Angeles%2C+CA%2C+USA!5e0!3m2!1sen!2sbd!4v1531805451146" style={{border: 0}} allowFullScreen width={600} height={450} />
               </div>
-              
+              <Image className="img-fluid" filename="mapa.jpg" alt="reviewer image" />
             </div>
             {/* End of .tab-content */}
             <div className="contact-info">
