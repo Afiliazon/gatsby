65258fb63ff818cb41c970d07379517e493e2d23
Afiliazon <info@afiliazon.com>
2020-03-31 18:04:39 +0200

    prueba

--- src/components/header.js
+++ src/components/header.js
@@ -65,11 +65,7 @@ const Header = ({ siteTitle }) => (
           </div>
           {/* End of .container */}
           <Helmet>
-          <script
-          src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-          integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-          crossOrigin="anonymous"
-        ></script>
+          
     <script src={withPrefix('jquery-migrate.min.js')} type="text/javascript" />
     <script src={withPrefix('easing-1.3.js')} type="text/javascript" />
     <script src={withPrefix('bootstrap.bundle.min.js')} type="text/javascript" />
